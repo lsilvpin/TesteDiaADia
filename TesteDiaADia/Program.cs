2beaaf9c619649280c8b04646c6546a8a78a8212
lsilvpin <lsilvpin@everis.com>
2020-09-17 19:51:55 -0300

    Convertendo string para o "tipo que eu quiser"

--- TesteDiaADia/Program.cs
+++ TesteDiaADia/Program.cs
@@ -6,15 +6,16 @@ namespace TesteDiaADia
     {
         static void Main(string[] args)
         {
-            // Entendendo os Enums
+            #region Entendendo Enums
             Classe classe = new Classe();
 
             Console.WriteLine($"Numero: {classe.Numero}");
             Console.WriteLine($"Tipo: {classe.Tipo}");
             Console.WriteLine($"Validação: {classe.Tipo == 0}");
             Console.WriteLine($"Nome da classe: {nameof(Classe)}");
+            #endregion
 
-            // Entendendo ref e out
+            #region Entendendo keywords: ref e out
             int x = 0;
             UpdateValue(ref x);
             Console.WriteLine($"x = {x}");
@@ -22,6 +23,44 @@ namespace TesteDiaADia
             Console.WriteLine("Nova branch");
 
             Console.WriteLine("Conteúdo para stash");
+            #endregion
+
+            #region Convertendo string para "tipo que eu quiser"
+            int integer = ConvertTo<int>("1");
+            Console.WriteLine($"Saída: {integer}");
+
+            classe = ConvertTo<Classe>("10");
+            Console.WriteLine($"Saída: {classe.Numero}");
+            #endregion
+
+        }
+
+        /// <summary>
+        /// Converte a entrada para o tipo passado por generics
+        /// </summary>
+        /// <typeparam name="T">Tipo informado</typeparam>
+        /// <param name="outVariable">Variável externa que receberá resultado</param>
+        /// <param name="text">Texto de entrada</param>
+        /// <returns>true se a conversão funcionou, false caso contrário</returns>
+        private static dynamic ConvertTo<T>(string text)
+        {
+            dynamic response = 0;
+
+            Type type = typeof(T);
+
+            if (type == typeof(int))
+            {
+                response = Int32.Parse(text);
+            }
+            else if (type == typeof(Classe))
+            {
+                response = new Classe()
+                {
+                    Numero = Int32.Parse(text)
+                };
+            }
+
+            return response;
         }
 
         /// <summary>
